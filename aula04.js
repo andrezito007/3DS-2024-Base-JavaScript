b0c201ec5cb53369aa94e78863d1e55e721052c5
andrezito <134400745+andrezito007@users.noreply.github.com>
2024-08-22 12:10:00 -0300

    Add files via upload

--- aula04.js
+++ aula04.js
@@ -45,4 +45,31 @@ imprimeTexto(multiplicacao())
 function multiplicacao(numero4 = 2,  numero5 = 5){
     return numero4 * numero5
 }
+
 imprimeTexto(multiplicacao(somaDeNumeros(3,4), somaDeNumeros(5,10)));
+
+// expressão de função
+
+const constanteSoma = function(nume1, nume2){return nume1 + nume2}
+
+console.log(constanteSoma(3,6))
+
+// Funções e var são "Listadas" no topo do arquivo
+
+// Arrow function
+
+const apresentaArrow = nome => `meu nome é ${nome}`;
+const somaEmFlecha = (nu1, nu2) => nu1 + nu2;
+
+console.log(apresentaArrow("André"))
+console.log(somaEmFlecha(2,4))
+
+const SomaNumeroPequenos = (num1, num2) => {
+    if(num1 > 10 || num2 > 10){
+        return "Essa função é apenas para números pequenos"
+    } else {
+        return num1 + num2 
+    }
+}
+
+console.log(SomaNumeroPequenos(2,3))
